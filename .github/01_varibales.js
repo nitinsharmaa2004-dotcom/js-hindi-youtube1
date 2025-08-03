2b5dc391c39cf8eb670820b56b82b8cdadcead2f
nitinsharmaa2004-dotcom <nitinsharmaa2004@gmail.com>
2025-08-03 15:58:46 +0530

    updated

---
+++ .github/01_varibales.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail ="nanu@google.com"
+var accountpassword ="12345"
+accountcity = "delhi"
+
+// accountID = 2 // not allowed
+
+
+
+console.log(account Id );
+
+
+
